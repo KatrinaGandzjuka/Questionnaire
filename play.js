59ad4b9dd86507938cb12bbfed16ea2339402d6b
KatrinaGandzjuka <78017510+KatrinaGandzjuka@users.noreply.github.com>
2023-08-02 09:38:01 +0300

    Update play.js

--- play.js
+++ play.js
@@ -54,7 +54,7 @@ startGame = () => {
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", `${score}/${MAX_QUESTIONS}`);
-    return window.location.assign("endscreen.html");
+    return window.location.assign("../endscreen.html");
   }
   questionCounter++;
   progress.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
